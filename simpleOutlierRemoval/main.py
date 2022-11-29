3d6e7ba981ae19b739b4ae767893c970c39e5a05
arjunjanamatti <arjunjanamatti@gmail.com>
2022-11-29 08:38:12 +0530

    updated main script with main functions

--- simpleOutlierRemoval/main.py
+++ simpleOutlierRemoval/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class removeOutlier:
+    '''
+    Use this package only if the assumption is that series is normally distributed.
+    Also it will return only the elements which are not outliers
+    '''
+    def __init__(self, data_series):
+        '''
+        :param data_series: list or pandas series
+        '''
+        self.series_1 = np.array(data_series)
 
+    def outlier_treatment_lower_upper(self):
+        mean = np.mean(self.series_1)
+        Q1 = np.percentile(self.series_1, 25, interpolation='midpoint')
+        Q3 = np.percentile(self.series_1, 75, interpolation='midpoint')
+        IQR = Q3 - Q1
+        self.low_lim = mean - 3 * (IQR)
+        self.up_lim = mean + 3 * (IQR)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def get(self):
+        self.outlier_treatment_lower_upper()
+        b = np.where(self.series_1 < self.up_lim & self.series_1 > self.low_lim)
+        return self.series_1[b]
+    
